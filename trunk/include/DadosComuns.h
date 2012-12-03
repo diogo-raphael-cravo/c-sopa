9d586c947c86902c77dc829cbcecd8b52650a483
diogo.raphael.cravo <diogo.raphael.cravo@20b5aa7f-cb87-d05e-307a-ade9354aea12>
2012-12-03 02:32:13 +0000

    Possibilidade de acessar o console com (KEY_BACKSPACE) e escrever no console. Para sair, é só digitar KEY_ENTER.

--- trunk/include/DadosComuns.h
+++ trunk/include/DadosComuns.h
@@ -30,6 +30,8 @@ pthread_t global_threadIdConsole;
 
 sem_t global_mutexAcessoTela; //Controla acesso à tela em geral.
 sem_t global_mutexParaTela; //Controla acesso à tela somente dos elementos que não são tela.
+sem_t global_mutexParaEscritaTela; //Controla acesso da tela à escrita na tela.
+sem_t global_mutexParaEscritaConsole; //Controla acesso do console à escrita na tela.
 
 PROCESSADOR 				global_processador;
 KERNEL 						global_kernel;
