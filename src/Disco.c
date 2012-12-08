dcf851d5dd614edfed5069e4d04abdb1b87cfe33
diogo.raphael.cravo <diogo.raphael.cravo@20b5aa7f-cb87-d05e-307a-ade9354aea12>
2012-12-08 16:58:02 +0000

    Disco faz leitura de arquivos.

--- src/Disco.c
+++ src/Disco.c
@@ -20,6 +20,8 @@
 */
 void disco_rodar(DISCO *disco_param){
 	sem_init(&disco_param->mutexAcessoDisco, 0, 0);
+	disco_carregar(disco_param, CAMINHO_ARQUIVO_BASE_DISCO);
+
 	while(1){
 		sem_wait(&disco_param->mutexAcessoDisco);
 		controladorInterrupcoes_set(&global_controladorInterrupcoes, INTERRUPCAO_DISCO);
@@ -33,4 +35,35 @@ void disco_rodar(DISCO *disco_param){
 */
 void disco_darUmaVolta(DISCO *disco_param){
 	sem_post(&disco_param->mutexAcessoDisco);
+}
+
+/**
+* Carrega os dados do arquivo, juntando-os aos dados que estão no disco.
+* Os dados são colocados à partir da primeira posição livre neste disco.
+* @param DISCO	*disco_param			O disco em que a operação será realizada.
+* @param char*	*caminhoArquivo_param	Arquivo que contém os dados, formatados da seguinte forma:
+*										J P A 0
+*										255 255 255 255
+*										0 0 0 0
+*/
+void disco_carregar(DISCO *disco_param, char *caminhoArquivo_param){
+	char linha[200];
+	char* byte;
+	char mensagem[200];
+	FILE *arquivoLido;
+	arquivoLido = fopen(caminhoArquivo_param, "r");
+	if(arquivoLido == NULL){
+		sprintf(mensagem, "Nao consegui carregar o arquivo '%s'.", caminhoArquivo_param);
+		tela_escreverNaColuna(&global_tela, mensagem, 4);
+	} else {
+		while(fgets(linha, 200, arquivoLido)){
+			byte=strtok(linha, " \n");
+			do{
+				if(byte != NULL){
+					sprintf(mensagem, "Acabei de ler '%s'.", byte);
+					tela_escreverNaColuna(&global_tela, mensagem, 4);
+				}
+			}while(byte=strtok(NULL," \n"));
+		}
+	}
 }
