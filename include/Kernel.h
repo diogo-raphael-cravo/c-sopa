52646beb34f6bfee7438c92b4baf8e4eef7cfe3b
diogo.raphael.cravo <diogo.raphael.cravo@20b5aa7f-cb87-d05e-307a-ade9354aea12>
2012-12-12 16:11:59 +0000

    Enderecamento logico implementado e funcionando.

--- include/Kernel.h
+++ include/Kernel.h
@@ -9,19 +9,12 @@
 #define MAXIMO_PROCESSOS_KERNEL 10
 #define DESCRITOR_PROCESSO_INEXISTENTE NULL
 
-struct str_alocacaoMemoria{
-	int enderecoInicio; //Início do programa na memória.
-	int tamanhoAreaMemoriaPalavras; //Tamanho da área de memória reservada ao programa, em palavras.
-};
-
-typedef struct str_alocacaoMemoria ALOCACAO_MEMORIA;
-
 struct str_kernel{
 	FIFO filaProcessosBloqueados; //Indica os índices de descritoresProcessos que contém processos bloqueados.
 									//Os processos são ordenados em uma fila por ordem de "chegada".
 	FIFO filaProcessosProntos; //Indica os índices de descritoresProcessos que contém processos prontos para rodar.
 								//Os processos são ordenados em uma fila por ordem de "chegada".
-	FIFO filaMemoriaAlocada; //Representa o que foi alocado de memória, com elementos ALOCACAO_MEMORIA.
+	MAPA_ALOCACOES_MEMORIA mapaMemoriaAlocada; //Representa o que foi alocado de memória, permitindo controle de endereçamento lógico.
 	DESCRITOR_PROCESSO** processoRodando; //Ponteiro para o processo que está rodando.
 	int quantidadeProcessos; //A quantidade de processos no momento.
 };
